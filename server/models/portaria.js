f308cf7e2acfcf84be318d340a3e4d4d77887eec
Eduardo Model <edu_model@hotmail.com>
2018-09-05 23:12:40 -0300

    Criado o primeiro modelo para salvar as informações referentes a portaria no servidor

---
+++ server/models/portaria.js
@@ -0,0 +1,46 @@
+let mongoose = require('mongoose')
+
+//define o corpo dos objetos que serão armazenados dentro da coleção
+let Portaria = mongoose.model('Portaria', {
+	estado: {
+		type: String,
+		minlength: 2,
+		maxlength: 2,
+		required: true
+
+	},
+	cidade: {
+		type: String,
+		required: true
+	},
+	bairro: {
+		type: String,
+		required: true
+	},
+	portaria: {
+		nome: {
+			type: String,
+			required: true
+		},
+		numero:{
+			type: Number,
+			required: true
+		},
+		ID:{
+			type: String,
+			minlength: 3,
+			maxlength: 3,
+			required: true
+		}
+	}
+	latitude: {
+		type: Number,
+		required: true
+	},
+	longitude: {
+		type: Number,
+		required: true
+	}
+})
+
+module.exports = {Log}
